4c073889a8f29e48c3552ebde9962e993510bd8d
deniPamungkas <pamungkasdeni18@gmail.com>
2023-12-16 21:25:04 +0700

    sudah

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.listen(PORT, () => {
 });
 
 const corsOption = {
-  origin: "https://fun-blog-p4woogziw-deni-pamungkas-projects.vercel.app/",
+  origin: "https://fun-blog-p4woogziw-deni-pamungkas-projects.vercel.app",
   credentials: true,
 };
 
