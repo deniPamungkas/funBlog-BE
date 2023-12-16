4afac5cd26402c6783f86a646ffbc080db7ebcd5
deniPamungkas <pamungkasdeni18@gmail.com>
2023-12-16 15:14:56 +0700

    upload

--- routes/uploadFile.js
+++ routes/uploadFile.js
@@ -5,7 +5,7 @@ const router = Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/public/uploads");
+    cb(null, "/uploads");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
